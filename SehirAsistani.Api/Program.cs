cf775857ed7bd137940d61a001fefd81d0588bdf
elif_ <elifery06@yandex.com>
2025-07-22 10:53:47 +0300

    şikayetservis ve controller 5

--- SehirAsistani.Api/Program.cs
+++ SehirAsistani.Api/Program.cs
@@ -76,7 +76,7 @@ builder.Services.AddScoped<AuthService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.Configure<SmtpSettings>(builder.Configuration.GetSection("Smtp"));
 builder.Services.AddScoped<ISmtpService, SmtpService>();
-builder.Services.AddScoped<IDuyguAnaliz, IDuyguAnaliz>();
+builder.Services.AddScoped<IDuyguAnaliz, DuyguAnalizService>();
 builder.Services.AddScoped<ISikayetService,SikayetService>();
 builder.Services.AddScoped<JwtService>();
 builder.Services.AddScoped<EmailService>();
