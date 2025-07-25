183beacb21deb007667c40acadd890c711957d30
Umut Güven <usluumutguven@gmail.com>
2025-07-25 22:35:23 +0300

    date3

--- SehirAsistanim.Infrastructure/Services/SikayetService.cs
+++ SehirAsistanim.Infrastructure/Services/SikayetService.cs
@@ -88,7 +88,7 @@ namespace SehirAsistanim.Infrastructure.Services
             sikayet.Durum = durum;
             if (durum == sikayetdurumu.Cozuldu)
             {
-                sikayet.CozulmeTarihi = DateTime.Now;
+                sikayet.CozulmeTarihi = DateTime.UtcNow.ToUniversalTime();
 
             }
             else
