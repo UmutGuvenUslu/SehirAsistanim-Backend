130a1fe027c6e188b26b11bc784e4e630260cb9b
Umut Güven <usluumutguven@gmail.com>
2025-07-28 07:17:39 +0300

    email

---
+++ SehirAsistanim.Domain/Dto's/ComplaintSolutionNotificationRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SehirAsistanim.Domain.Dto_s
+{
+    public class ComplaintSolutionNotificationRequest
+    {
+
+        [Required(ErrorMessage = "Kullanıcı ID zorunludur")]
+        public int KullaniciId { get; set; }
+
+        [Required(ErrorMessage = "Şikayet ID zorunludur")]
+        public int SikayetId { get; set; }
+    }
+}
